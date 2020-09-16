1a25cb99c9c8dbe3598fa3a3eb5fda0b42428aaf
EC2 Default User <ec2-user@ip-172-31-22-18.us-east-2.compute.internal>
2020-09-16 01:38:06 +0000

    part 3 complete, hopefully

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  
+  helper_method :picked_rating?
   helper_method :hilight
   
   def movie_params
@@ -13,17 +13,32 @@ class MoviesController < ApplicationController
   end
 
   def index
+    # everytime params is updated and not null, update sessions
+    if !params[:ratings].nil?
+      session[:ratings] = params[:ratings]
+    end
+    if !params[:order].nil?
+      session[:order] = params[:order] unless params[:order].nil?
+    end
+    # get the unique rating, notice that if there is only 4 out of 5 rating available in movies, the check box will only show 4 as well
     arrays=[]
     temprats=Movie.all
     temprats.each do |movie|
       arrays.append(movie.rating)
     end
     @all_ratings=arrays.uniq
-    if params[:ratings].nil?
-      @movies = Movie.order params[:order]
+    
+    if (params[:ratings].nil? && !session[:ratings].nil?) || (params[:order].nil? && !session[:order].nil?)
+      redirect_to movies_path("ratings" => session[:ratings], "order" => session[:order])
+    elsif !params[:ratings].nil? || !params[:order].nil?
+      if !params[:ratings].nil?
+        array_ratings = params[:ratings].keys
+        return @movies = Movie.where(rating: array_ratings).order(session[:order])
+      else
+        return @movies = Movie.all.order(session[:order])
+      end
     else
-      array_ratings = params[:ratings].keys
-      @movies = Movie.where(rating: array_ratings).order params[:order]
+      return @movies = Movie.all
     end
   end
 
@@ -56,11 +71,17 @@ class MoviesController < ApplicationController
   end
   
   def hilight(column)
-    if(params[:order].to_s == column)
+    if(session[:order].to_s == column)
       return 'hilite'
     else
       return nil
     end
+  end
+  
+  def picked_rating?(rating)
+    picked_rating_arr = session[:ratings]
+    return true if picked_rating_arr.nil?
+    picked_rating_arr.include? rating
   end
 end
 
